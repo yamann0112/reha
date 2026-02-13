30946e468e5cb37f9efbb443bab58c7a17f95fa3
yamann0112 <erhanyaman1938@gmail.com>
2026-02-13 19:29:46 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,15 +61,7 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(httpServer, app);
-
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-    const status = err.status || err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-
-    res.status(status).json({ message });
-    throw err;
-  });
-
+  
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
